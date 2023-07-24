00c8b5c4984282641ae385d589a63dd1e0233a5b
mnmanuprasad <mnmanuprasad01@gmail.com>
2023-07-24 21:32:19 +0530

    shadow moueover event

---
+++ ticTacToe/script.js
@@ -0,0 +1,29 @@
+const elements = document.querySelectorAll('.move-shadow')
+
+const planetMove = 'planet'
+const starMove = 'star'
+
+let nextMove = planetMove
+
+
+
+
+function handleNextMove(e){
+   const shadowEle = e.target
+   console.log("shadow : ", shadowEle)
+   if(nextMove == planetMove){
+    shadowEle.className = 'move-shadow shadow-planet'
+    nextMove = starMove
+   }
+   else{
+    shadowEle.className = 'move-shadow shadow-star'
+    nextMove = planetMove
+   }
+ 
+  
+}
+
+elements.forEach((element)=>{
+    element.addEventListener('mouseover',handleNextMove)
+})
+
